6c529f718bbcc834df1d1df2d6bbc1e5e01e3eaa
Ayush Saraswat <ayushsara12@gmail.com>
2020-09-30 21:01:32 +0530

    basic layout added.

---
+++ src/MemoryManager.h
@@ -0,0 +1,46 @@
+// Header guard
+#pragma once
+
+namespace MM{
+  // allocating 100MB Memory Pool.
+  const int MM_POOL_SIZE = 1024*1000*100;
+
+  // Initialize any data needed to manage the memory pool
+  void init(void);
+
+  // return a pointer inside the memory pool
+  // If no chunk can accommodate aSize call OnAllocFail()
+  void* allocate(int aSize);
+
+  // Free up a chunk previously allocated
+  void deallocate(void* aPointer);
+
+  //---
+  //--- support routines
+  //--- 
+
+  // Will scan the memory pool and return the total free space remaining
+  int freeRemaining(void);
+
+  // Will scan the memory pool and return the largest free space remaining
+  int largestFree(void);
+
+  // will scan the memory pool and return the smallest free space remaining
+  int smallestFree(void);
+
+  //---
+  //--- error conditions. None of these functions will return
+  //--- These routines do not need to be implemented by the candidate
+  //---
+
+  // Call if no space is left for the allocation request
+  void onOutOfMemory(void);
+
+  // If the caller makes any illegal request your code should call this 
+  // provided failure function (which will not return):
+  void onIllegalOperation(const char* fmt,...);
+  // eg:
+  //   int errorCode;
+  //   ...
+  //   onIllegalOperation("Error in createQueue: %d", errorCode);
+};
